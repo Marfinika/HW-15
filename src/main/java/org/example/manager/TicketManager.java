9b4118318d06f551d90a86a6bcb0faf5bf010bd4
Ivan Zuev <ivan.zuev96@yandex.ru>
2022-12-05 20:30:48 +0300

    first commit

---
+++ src/main/java/org/example/manager/TicketManager.java
@@ -0,0 +1,41 @@
+package org.example.manager;
+
+import org.example.domain.Ticket;
+import org.example.repository.TicketRepository;
+
+import java.util.Arrays;
+
+public class TicketManager {
+    private TicketRepository repo;
+
+    public TicketManager(TicketRepository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Ticket ticket) {
+        repo.save(ticket);
+    }
+
+    public Ticket[] searchBy(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repo.findAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp [i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public boolean matches(Ticket ticket, String searchFrom, String searchTo) {
+        if (ticket.getFrom().contains(searchFrom) & ticket.getTo().contains(searchTo)) {
+            return true;
+        }
+        return false;
+    }
+}
